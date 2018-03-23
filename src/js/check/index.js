15e0386e3f137328aceacdfb5ee79ea235c0aded
RosyG <rmga2106@gmail.com>
2018-03-22 22:49:04 -0600

    Agregando funcionalidad de filtro

--- src/js/check/index.js
+++ src/js/check/index.js
@@ -1,11 +1,18 @@
 let borrar = require('../erase');
-const finishTasks = require('../tasks');
+const tasksType = require('../tasks');
+
 
 
 const check = {
     init() {
         console.log('Hola bundle!!');
         $('#todo-form').submit(check.addTodo);
+        $('#active').click(tasksType.active);
+        $('#finish').click(tasksType.finish);
+        $('#active').dblclick(tasksType.allTasks);
+        $('#finish').dblclick(tasksType.allTasks);
+
+
     },
     addTodo(event) {
         event.preventDefault();
@@ -16,7 +23,7 @@ const check = {
 
         const $todoContainer = $('<div />').addClass('col s12 m12 todo');
         const $todoCard = $('<div />').addClass('card-panel');
-        const $todoCheckbox = $('<input type="checkbox" />').attr('id', todoId);
+        const $todoCheckbox = $('<input type="checkbox" />').addClass('all-tasks').attr('id', todoId);
         const $todoText = $('<label />').attr('for', todoId).text(todo);
         const $row = $("<div />").addClass("row");
         const $erase = $("<a />").addClass("col s2 m2 btn right").text("x").click(borrar);
@@ -27,12 +34,13 @@ const check = {
         $todoCard.append($todoText);
         $todoContainer.append($todoCard);
 
-        $('#todos').append($todoContainer);//Línea que borra lo que se introdujo al textarea.
-
+        $('#todos').append($todoContainer);
+        document.getElementById("todo").value = "";//Limpiando textArea.
         check.counter = check.counter + 1;
 
     },
     counter: 0
+
 };
 
 module.exports = check;
