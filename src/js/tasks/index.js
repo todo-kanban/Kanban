f6054b42626293dca5981453deaa453afa61bd1d
RosyG <rmga2106@gmail.com>
2018-03-22 20:22:59 -0600

    Agregando botones

---
+++ src/js/tasks/index.js
@@ -0,0 +1,17 @@
+const finish = () => console.log('finish tasks');
+
+module.exports = finish;
+
+//
+// const tasks = {
+//   init() {
+//       $('input').click(tasks.finishTasks);
+//   },
+//   finishTasks(e){
+//     e.preventDefault();
+//     console.log('buton');
+//   }
+// }
+//
+//
+// module.exports = tasks;
